050b49ca199415b92ddcb11cc73abb2a153936c3
makokhamj <user@users-MBAir.local>
2015-09-02 22:09:28 -0700

    After Adding Dummy Data

--- app/src/main/java/com/josephmakokha/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/josephmakokha/sunshine/app/MainActivityFragment.java
@@ -6,17 +6,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
     public MainActivityFragment() {
     }
+        @Override
+         public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            String[] data = {
+                    "Mon 8/30 - Sunny - 63/78",
+                    "Tue 9/1 - Foggy - 60/68",
+                    "Wed 9/2 - Rainy - 67/72",
+                    "Thur 9/3 - Foggy - 55/67",
+                    "Fri 9/4 - Sunny - 69/88",
+                    "Sat 9/5 - Trapped in Weatherstation - 48/58",
+                    "Sun 9/6 - Foggy - 79/84",
+            };
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
 }
